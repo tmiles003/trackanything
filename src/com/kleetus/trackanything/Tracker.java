d239ffbf4f9a8be321622e4cb8f0ded4c5912211
Chris Kleeschulte <ckleeschulte@rentpath.com>
2013-08-26 19:46:06 -0400

    Added database class

--- src/com/kleetus/trackanything/Tracker.java
+++ src/com/kleetus/trackanything/Tracker.java
@@ -2,5 +2,30 @@ package com.kleetus.trackanything;
 
 
 public class Tracker {
-    
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+
 }
