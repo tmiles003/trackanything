739b1c52d0d840f11a34a49a0dbb0c9ff12f25af
Chris Kleeschulte <ckleeschulte@rentpath.com>
2013-09-05 15:58:39 -0400

    Added nav drawer operation.

--- src/com/kleetus/trackanything/Main.java
+++ src/com/kleetus/trackanything/Main.java
@@ -1,16 +1,54 @@
 package com.kleetus.trackanything;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
 
 public class Main extends TrackAnything {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    CharSequence drawerTitle;
+    CharSequence title;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main);
+
+        title = drawerTitle = getTitle();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+
+            public void onDrawerClosed(View view) {
+
+                getActionBar().setTitle(title);
+                invalidateOptionsMenu();
+
+            }
+
+            public void onDrawerOpened(View view) {
+
+                getActionBar().setTitle(drawerTitle);
+                invalidateOptionsMenu();
+
+            }
+
+        };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
 
         load_tracker_fragment();
 
@@ -33,6 +71,30 @@ public class Main extends TrackAnything {
         ft.add(R.id.list_frame, trackerFragment).commit();
 
     }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 
 }
--- src/com/kleetus/trackanything/TrackAnything.java
+++ src/com/kleetus/trackanything/TrackAnything.java
@@ -1,12 +1,24 @@
 package com.kleetus.trackanything;
 
 
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 public class TrackAnything extends ActionBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
